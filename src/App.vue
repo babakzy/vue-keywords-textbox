cc9336d95d394ad5a4347ca6af25f9fe990283af
Babak <babaksdz@gmail.com>
2023-01-09 17:52:44 +0330

    add local files

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="container">
+    <div class="row my-5  justify-content-center">
+      <div class="col-md-12">
+        <h4 class="my-4 text-center">Keywords Textbox</h4>
+      </div>
+      <div class="col-md-4">
+        <label for="text" class="mb-2">Type Keyword and Seperate Them by Comma Symbol (,)  </label>
+        <input type="text" v-model="textInput" @keyup="textChanged" class="text-input mb-4 w-100">
+        <span class="keyword-item" v-for="item in keywords" :key="item">{{ item }}</span>
+      </div>
+    </div>
+
+  </div>
+
 </template>
+<script >
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'App',
+  components: {
 
+  },
+  data: function () {
+    return {
+      keywords: [],
+      textInput: ''
+    }
+
+  },
+  mounted() {
+    console.log('Mounted');
+  },
+  methods: {
+    textChanged() {
+      if (this.textInput.indexOf(',') > 0 && this.textInput.length) {
+        let keyword = this.textInput.replace(',', '')
+        this.keywords.push(keyword);
+        this.textInput = ''
+      }
+    }
+  }
+});
+
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.keyword-item {
+  display: inline-block;
+  margin-top: 5px;
+  margin-right: 7px;
+  background-color: rgb(219, 219, 219);
+  padding: 4px;
+  border-radius: 4px;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
